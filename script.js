7e503a3e47f4fb0f8ed9a8d55294ea8578c64319
lmadhu7 <madhusmile226@gmail.com>
2024-07-21 12:42:37 +0530

    reCAPTHCHA changes

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function beforesubmit() {
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
-  console.log("inputdate.value", inputdate.value); //string --> date(en_IN)
+let capthchaChecked = false;
+function beforesubmit(event) {
+  if (capthchaChecked) {
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate.value", inputdate.value); //string --> date(en_IN)
 
-  let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-  outputdate.value = formatedDate;
+    let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formatedDate;
+  } else {
+    alert("Please check the reCAPTHCHA box to submit the lead");
+    event.preventDefault();
+  }
 }
+
 function timestamp() {
   var response = document.getElementById("g-recaptcha-response");
   if (response == null || response.value.trim() == "") {
@@ -18,3 +25,6 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+function capthchasuccess() {
+  capthchaChecked = true;
+}
