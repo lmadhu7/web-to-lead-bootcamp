3e62dec246b2eb4c6ffd646c70b87180e8245298
lmadhu7 <madhusmile226@gmail.com>
2024-07-21 11:42:29 +0530

    github link added

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function beforesubmit() {
   let outputdate = document.querySelector(".outputdate");
   let inputdate = document.querySelector(".inputdate");
+  console.log("inputdate.value", inputdate.value); //string --> date(en_IN)
+
+  let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  outputdate.value = formatedDate;
 }
